1f0e64eeb3e35ffad39450f63c59ac20e533b485
ster ster <76195078+Fish-Sticks@users.noreply.github.com>
2024-09-24 16:42:41 -0500

    Removed the hardcoded base of code

--- roblox_decryptor/decryptor/decryptor.cpp
+++ roblox_decryptor/decryptor/decryptor.cpp
@@ -26,13 +26,13 @@ namespace decryptor
 		const auto hyperion_code = hyperion_image.get_section(".byfron");
 
 		DWORD old;
-		VirtualProtect(reinterpret_cast<LPVOID>(roblox_code.base), roblox_code.size, PAGE_READWRITE, &old);
+		VirtualProtect(reinterpret_cast<LPVOID>(roblox_code.virtual_range.base), roblox_code.virtual_range.size, PAGE_READWRITE, &old);
 
 		// Attempt to automatically locate the page info array
 		constexpr std::array<std::uint8_t, 6> constant_sig = { 0x10, 0x27, 0x00, 0x00, 0xCC, 0x29 };
 		constexpr std::array<std::uint8_t, 3> lea_sig = { 0x04, 0xCC, 0x8D };
 
-		const auto constant_mov = utils::signature_scan(hyperion_code.base, hyperion_code.size, constant_sig);
+		const auto constant_mov = utils::signature_scan(hyperion_code.virtual_range.base, hyperion_code.virtual_range.size, constant_sig);
 
 		if (!constant_mov)
 			return;
@@ -45,11 +45,11 @@ namespace decryptor
 		constexpr std::array<std::uint8_t, 16> short_sig = { 0xBA, 0xCC, 0xCC, 0xCC, 0xCC, 0x45, 0xCC, 0xCC, 0x48, 0x8D, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xEB };
 		constexpr std::array<std::uint8_t, 17> long_sig = { 0x41, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0x45, 0xCC, 0xCC, 0x48, 0x8D, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xEB };
 
-		auto int3_info = utils::signature_scan(hyperion_code.base, hyperion_code.size, long_sig);
+		auto int3_info = utils::signature_scan(hyperion_code.virtual_range.base, hyperion_code.virtual_range.size, long_sig);
 		is_long_info = true;
 
 		if (!int3_info) {
-			int3_info = utils::signature_scan(hyperion_code.base, hyperion_code.size, short_sig);
+			int3_info = utils::signature_scan(hyperion_code.virtual_range.base, hyperion_code.virtual_range.size, short_sig);
 			if (!int3_info) {
 				std::printf("Failed to locate INT3 info!\n");
 				return;
@@ -97,7 +97,7 @@ namespace decryptor
 		const auto roblox_code = roblox_image.get_section(".text");
 		const auto hyperion_code = hyperion_image.get_section(".byfron");
 
-		for (auto target_page = roblox_code.base; target_page < roblox_code.base + roblox_code.size; target_page += 0x1000)
+		for (auto target_page = roblox_code.virtual_range.base; target_page < roblox_code.virtual_range.base + roblox_code.virtual_range.size; target_page += 0x1000)
 		{
 			const auto target_page_number = (target_page - roblox_image.get_image_base()) / 0x1000;
 			const auto target_page_info_base = page_info_base + (target_page_number % 10000) * 0x10;
@@ -115,10 +115,8 @@ namespace decryptor
 
 		decrypt_int3(); // Begin decrypting INT3 after the code.
 
-		// Seek to the first section after the PE headers, assuming it should be code
-		out_file.seekp(0x600);
-		out_file.write(reinterpret_cast<char*>(roblox_code.base), roblox_code.size);
-		out_file.flush();
+		out_file.seekp(roblox_code.raw_range.base);
+		out_file.write(reinterpret_cast<char*>(roblox_code.virtual_range.base), roblox_code.virtual_range.size);
 	}
 
 	void code_decryptor::decrypt_int3()
